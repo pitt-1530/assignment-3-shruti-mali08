9d04bfa8ce7232d8361ac3640a6ea6b49b427e7e
Shruti Mali <shrutijmali08@gmail.com>
2025-12-01 23:11:20 -0500

    Completed function implementations

--- src/main/java/edu/pitt/se/PlaylistRecommender.java
+++ src/main/java/edu/pitt/se/PlaylistRecommender.java
@@ -6,16 +6,60 @@ public class PlaylistRecommender {
 
     public static String classifyEnergy(List<Integer> bpms) {
         // TODO: Implement classifyEnergy()
-        throw new UnsupportedOperationException("Not implemented");
+
+        // checking for null or empty lists
+        if(bpms == null || bpms.isEmpty())
+        {
+            throw new UnsupportedOperationException("The list cannot be null or empty.");
+        }
+        
+        // calculating the average bpm
+        double sum = 0;
+        for(int bpm : bpms)
+        {
+            sum += bpm;
+        }
+
+        double avg = sum/bpms.size();
+
+        if(avg >= 140)
+            return "HIGH";
+        else if(avg < 100)
+            return "LOW";
+        else
+            return "NEDIUM";
     }
 
     public static boolean isValidTrackTitle(String title) {
         // TODO: Implement isValidTrackTitle()
-        throw new UnsupportedOperationException("Not implemented");
+        // checking null or empty string
+        if(title == null || title.isEmpty())
+            return false;
+
+        // check string length
+        if((title.length() < 1) || (title.length()> 30))
+            // throw new UnsupportedOperationException("String should be of length 1 to 30");
+            return false;
+
+        // checking for special characters
+        if(!title.matches("[a-zA-Z0-9 +]+"))
+            return false;
+
+        // throw new UnsupportedOperationException("Not implemented");
+        return true; 
     }
 
     public static int normalizeVolume(int volumeDb) {
         // TODO: Implement normalizeVolume()
-        throw new UnsupportedOperationException("Not implemented");
+
+        if(volumeDb > 100){
+            volumeDb = 100;
+        }
+        else if(volumeDb < 0){
+            volumeDb = 0;
+        }
+
+        return volumeDb;
+        // throw new UnsupportedOperationException("Not implemented");
     }
 }
